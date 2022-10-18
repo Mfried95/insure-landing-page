57eca1aea9879138a5de0a062ee1dfcbe831a89c
Mfried <81455905+Mfried95@users.noreply.github.com>
2022-10-18 13:09:39 -0500

    Finished desktop and mobile navigation button/ dropdown menu
    - Added drop down menu in media query using useRef hook
    
    - styled components for mobile and desktop
    
    Next: Need to finish styling mobile and desktop navigation

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,19 +1,37 @@
 import React from "react";
 import "../Styles/navigation.css";
 import Logo from "../images/logo.svg";
-import Hamburger from "../images/icon-hamburger.svg";
+import Hamburger from "../images/icon-hamburger.svg"
+import {useRef} from "react";
 
 function Navigation() {
-  return (
-    <>
-      <nav>
-        <img className="Logo" src={Logo} alt="logo" />
 
-        <div className="hamburger-mobile">
-          <img src={Hamburger} alt="hamburger menu" />
+const navRef = useRef();
+
+const showNavbar = () => {
+    navRef.current.classList.toggle("responsive-nav")
+}
+
+  return (
+    <header>
+        <img src={Logo} alt="" />
+
+        <nav ref={navRef}>
+            <a href="/#">How we work</a>
+            <a href="/#">Blog</a>
+            <a href="/#">Account</a>
+            <a href="/#">View Plans</a>
+
+            <div className="nav-btn nav-close-btn" onClick={showNavbar}>
+                <img src={Hamburger} alt="" />
+            </div>
+        </nav>
+
+        <div className="nav-btn" onClick={showNavbar}>
+                <img src={Hamburger} alt="" />
         </div>
-      </nav>
-    </>
+        
+    </header>
   );
 }
 

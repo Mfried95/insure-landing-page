e4c1628acf14c564b04309130a02ddcb93bb0556
Mfried <81455905+Mfried95@users.noreply.github.com>
2022-10-19 13:28:10 -0500

    Added hero section
    - Sized background img for mobile and desktop

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import '../Styles/hero.css'
+
+
+const Hero = () => {
+  return (
+    <section className='hero-container'>
+
+        
+
+        <div className='hero-article'>
+            <h1>Humanizing your insurance.</h1>
+            <p>Get your life insurance coverage easier and faster. We blend our expertise and techonology to help you find the plan thats right for you. Ensure your and your loved ones are protected.</p>
+            <button>View Plans</button>
+        </div>
+
+        <div className='hero-img'>
+            
+        </div>
+
+    </section>
+  )
+}
+
+export default Hero

0ca7fff890999f3480d1b7cf713e79947590ce91
Mfried <81455905+Mfried95@users.noreply.github.com>
2022-10-19 19:20:00 -0500

    Added new component
    - Added "Mid componenet"
    - Added styles for mid componenet

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Navigation from './Components/Navigation';
 import Hero from './Components/Hero';
-
+import Middle from './Components/Middle';
 
 function App() {
   return (
     <div className="App">
      <Navigation/>
      <Hero/>
+     <Middle/>
     </div>
   );
 }
---
+++ src/Components/Middle.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Styles/mid.css'
+
+
+const Middel = () => {
+  return (
+    <div className='mid-container'>
+      <div className='line'></div>
+      <h1>We're diffrent</h1>
+    </div>
+  )
+}
+
+export default Middel

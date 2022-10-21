425fbfb31e4ad20ce51b50086b06efb026636630
Mfried <81455905+Mfried95@users.noreply.github.com>
2022-10-21 13:25:30 -0500

    Updated Hero container
    - Changed hero-content sizing to fit with desktop design only

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -9,9 +9,13 @@ const Hero = () => {
         
 
         <div className='hero-article'>
-            <h1>Humanizing your insurance.</h1>
+
+          <div className="hero-content">
+          <h1>Humanizing your insurance.</h1>
             <p>Get your life insurance coverage easier and faster. We blend our expertise and techonology to help you find the plan thats right for you. Ensure your and your loved ones are protected.</p>
             <button>View Plans</button>
+          </div>
+            
         </div>
 
         <div className='hero-img'>

04c58ffeb6b8637ef38bda441299c19a06b60bdc
Mfried <81455905+Mfried95@users.noreply.github.com>
2022-10-24 11:33:09 -0500

    Added images to footer conainter
    - Added logo
    - Added icon images

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import "../Styles/footer.css"
+import Logo from "../images/logo.svg";
+import Facebook from "../images/icon-facebook.svg"
+import Twitter from "../images/icon-twitter.svg"
+import Pinterest from "../images/icon-pinterest.svg"
+import Instagram from "../images/icon-instagram.svg"
 
 const Footer = () => {
   return (
     <div>
-        
+       <div className="footer-container">
+            <div className="logo-content">
+                <img src={Logo} alt="" />
+                    <div className="icons">
+                        <img src={Facebook} alt="" />
+                        <img src={Twitter} alt="" />
+                        <img src={Pinterest} alt="" />
+                        <img src={Instagram} alt="" />
+                        <div className="footer-line"></div>
+                    </div>
+            </div>
+        </div> 
     </div>
   )
 }

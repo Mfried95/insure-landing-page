5f0a5aa81b70209f82a500333947917ad8efe9c2
Mfried <81455905+Mfried95@users.noreply.github.com>
2022-10-24 12:23:46 -0500

    Styled links elements for mobile

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
     <div>
        <div className="footer-container">
             <div className="logo-content">
-                <img src={Logo} alt="" />
+                <img src={Logo} alt="" className='footer-logo' />
                     <div className="icons">
                         <img src={Facebook} alt="" />
                         <img src={Twitter} alt="" />
@@ -20,6 +20,42 @@ const Footer = () => {
                         <div className="footer-line"></div>
                     </div>
             </div>
+
+        <div className="links">
+            <div className="fot-card">
+                <h2>Our Company</h2>
+                <li><a href="/#">HOW WE WORK</a></li>
+                <li><a href="/#">WHY INSURE?</a></li>
+                <li><a href="/#">CHECK PRICE</a></li>
+                <li><a href="/#">REVIEWS</a></li>
+            </div>
+
+            <div className="fot-card">
+                <h2>Help Me</h2>
+            <li><a href="/#">FAQ</a></li>
+            <li><a href="/#">TERMS OF USE</a></li>
+            <li><a href="/#">COOKIES</a></li>
+            </div>
+
+            <div className="fot-card">
+                <h2>Contact</h2>
+            <li><a href="/#">SALES</a></li>
+            <li><a href="/#">SUPPORT</a></li>
+            <li><a href="/#">LIVE CHAT</a></li>
+            </div>
+
+            <div className="fot-card">
+                <h2>Others</h2>
+            <li><a href="/#">CAREERS</a></li>
+            <li><a href="/#">PRESS</a></li>
+            <li><a href="/#">LICESNSES</a></li>
+            </div>
+
+            <div className="fot-card">
+                
+            </div>
+        </div>
+
         </div> 
     </div>
   )
